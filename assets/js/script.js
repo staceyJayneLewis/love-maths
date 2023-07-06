b13aaff84180625d5ec1aa1af5de6b0968707661
Stacey Lewis <stacey.lws@googlemail.com>
2023-07-06 17:26:30 +0000

    Add code to calculate to random numbers for the game question.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,29 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for(let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit"){
+                alert("You clicked Submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+
+});
+
+/**
+ * The main game loop when the script is first started
+ * and after the users answer has been processed. 
+ */
 function runGame(){
 
+    //Creates random numbers between 1 and 25 //
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+    
 }
 
 function checkAnswer(){
@@ -27,5 +51,5 @@ function displaySubtractQuestion(){
 }
 
 function displayMultiplyQuestion(){
-    
+
 }
